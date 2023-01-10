66189701b7ae5f6bf60adef63c2dd09bce2537e3
Karla Desktop <8628261+aguantame@users.noreply.github.com>
2023-01-10 17:47:22 -0500

    version information updated

--- src/DetectorConstruction.cc
+++ src/DetectorConstruction.cc
@@ -19,39 +19,6 @@
 #include "Constants.hh"
 #include "ScintSD.hh"
 
-#include "G4RunManager.hh"
-#include "G4GeometryManager.hh"
-#include "G4PhysicalVolumeStore.hh"
-#include "G4LogicalVolumeStore.hh"
-#include "G4SolidStore.hh"
-
-#include "G4Material.hh"
-#include "G4NistManager.hh"
-
-#include "G4Box.hh"
-#include "G4Tubs.hh"
-#include "G4Sphere.hh"
-#include "G4Cons.hh"
-#include "G4UnionSolid.hh"
-#include "G4SubtractionSolid.hh"
-
-#include "G4LogicalVolume.hh"
-#include "G4PVPlacement.hh"
-#include "G4AutoDelete.hh"
-
-#include "G4UserLimits.hh"
-#include "G4SDManager.hh"
-
-#include "G4VisAttributes.hh"
-#include "G4Colour.hh"
-
-#include "G4PhysicalConstants.hh"
-#include "G4SystemOfUnits.hh"
-
-#include "G4AssemblyVolume.hh"
-
-#include "globals.hh"
-#include "G4VUserDetectorConstruction.hh"
 
 
 //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
